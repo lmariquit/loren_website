6655568d49fb4a518abbe0cc18e991f96fd0d45b
Loren Mariquit <l.mariquit@gmail.com>
2019-02-17 23:59:39 -0500

    footer added. linkedin and github icons added

---
+++ loren_website/client/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id="footer-container">
+      <i id="icon-linkedin" className="fa fa-linkedin" />
+      <i id="icon-github" className="fa fa-github" />
+      <div>Loren Manuel Cotaco Mariquit</div>
+    </div>
+  )
+}
+
+export default Footer
