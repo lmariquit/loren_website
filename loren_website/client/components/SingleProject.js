6f7bb7518fee55cc3da3863ceebca58f28c1968f
Loren Mariquit <l.mariquit@gmail.com>
2019-02-18 22:54:00 -0500

    made projects much bigger, currently all lined up to left. still need to line every other on the right side

--- loren_website/client/components/SingleProject.js
+++ loren_website/client/components/SingleProject.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const SingleProject = ({ title, image, description }) => {
+  // return (
+  //   <div id="single-proj-container">
+  //     <img src={image} />
+  //     <div id="proj-title">{title}</div>
+  //     <div id="proj-desc">{description}</div>
+  //   </div>
+  // )
   return (
     <div id="single-proj-container">
-      <img src={image} />
-      <div id="proj-title">{title}</div>
-      <div id="proj-desc">{description}</div>
+      <div id="proj-img-container">
+        <img src={image} />
+      </div>
+      <div id="proj-info">
+        <div id="proj-title">{title}</div>
+        <div id="proj-desc">{description}</div>
+      </div>
     </div>
   )
 }
