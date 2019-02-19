9fe4e77cf0502d93294092f621b453825c578f58
Loren Mariquit <l.mariquit@gmail.com>
2019-02-18 23:35:03 -0500

    div now extends full length. Info box for projects styled better

--- loren_website/client/components/SingleProject.js
+++ loren_website/client/components/SingleProject.js
@@ -10,10 +10,10 @@ const SingleProject = ({ title, image, description }) => {
   // )
   return (
     <div id="single-proj-container">
-      <div id="proj-img-container">
+      <div id="proj-img-container" className="one">
         <img src={image} />
       </div>
-      <div id="proj-info">
+      <div id="proj-info-container">
         <div id="proj-title">{title}</div>
         <div id="proj-desc">{description}</div>
       </div>
