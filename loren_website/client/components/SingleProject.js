884e9072a9a08613e3eb528c7aaa2cc18baa52b7
Loren Mariquit <l.mariquit@gmail.com>
2019-02-19 00:59:27 -0500

    slightly cleaned up. deleted some commented out code

--- loren_website/client/components/SingleProject.js
+++ loren_website/client/components/SingleProject.js
@@ -1,13 +1,6 @@
 import React from 'react'
 
 const SingleProject = ({ title, image, description }) => {
-  // return (
-  //   <div id="single-proj-container">
-  //     <img src={image} />
-  //     <div id="proj-title">{title}</div>
-  //     <div id="proj-desc">{description}</div>
-  //   </div>
-  // )
   return (
     <div id="single-proj-container">
       <div id="proj-img-container" className="one">
