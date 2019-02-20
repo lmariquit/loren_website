34b6dc5efc6904654635d45d81932fbf3ead3f7e
Loren Mariquit <l.mariquit@gmail.com>
2019-02-20 12:28:44 -0500

    intro text box styled and looks much better with the background

--- loren_website/client/components/Home.js
+++ loren_website/client/components/Home.js
@@ -3,7 +3,6 @@ import React from 'react'
 const Home = () => {
   return (
     <div id="intro-container">
-      <div id="intro-background" />
       <div id="intro-text">
         <div className="intro">
           <div id="intro-first-greeting">{`Hi, I\'m`}</div>
