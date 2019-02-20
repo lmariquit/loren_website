210d0486df67654bbc6dd410295e605c51e1cca4
Loren Mariquit <l.mariquit@gmail.com>
2019-02-20 12:10:10 -0500

    added skyline to background

--- loren_website/client/components/Home.js
+++ loren_website/client/components/Home.js
@@ -3,14 +3,17 @@ import React from 'react'
 const Home = () => {
   return (
     <div id="intro-container">
-      <div className="intro">
-        <div id="intro-first-greeting">{`Hi, I\'m`}</div>
-        <div id="intro-name">{`Loren Mariquit`}</div>
+      <div id="intro-background" />
+      <div id="intro-text">
+        <div className="intro">
+          <div id="intro-first-greeting">{`Hi, I\'m`}</div>
+          <div id="intro-name">{`Loren Mariquit`}</div>
+        </div>
+        <div
+          id="intro-second-greeting"
+          className="intro"
+        >{`Welcome to my Website!`}</div>
       </div>
-      <div
-        id="intro-second-greeting"
-        className="intro"
-      >{`Welcome to my Website!`}</div>
     </div>
   )
 }
