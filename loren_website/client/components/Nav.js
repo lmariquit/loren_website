e01805e3f16fb3182699826d68def469c9527709
Loren Mariquit <l.mariquit@gmail.com>
2019-02-20 21:36:44 -0500

    changed nav to class component and added state. nav items now grow when scrolled to a specific section

--- loren_website/client/components/Nav.js
+++ loren_website/client/components/Nav.js
@@ -1,26 +1,140 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { findDOMNode } from 'react-dom'
 
-const Nav = () => {
-  return (
-    <div id="nav-container">
-      <a id="nav-name" className="nav-item" href="#intro-container">
-        LOREN MARIQUIT
-      </a>
-      <a className="nav-item" href="#proj-container">
-        PROJECTS
-      </a>
-      <a className="nav-item" href="#skills-container">
-        SKILLS
-      </a>
-      <a className="nav-item" href="#contact-container">
-        CONTACT
-      </a>
-      <div className="nav-item">|</div>
-      <div className="nav-item" onClick={() => clicked()}>
-        ABOUT
+class Nav extends Component {
+  constructor() {
+    super()
+    this.navUnderline = this.navUnderline.bind(this)
+    this.state = {
+      scrollTop: 0,
+      modalVisible: false,
+      noButton: 0,
+      projectsButton: 0,
+      skillsButton: 0,
+      contactButton: 0,
+      aboutButton: 0
+    }
+  }
+
+  componentDidMount() {
+    this.state &&
+      window.addEventListener('scroll', this.onScroll.bind(this), false)
+  }
+
+  componentDidUpdate() {
+    console.log('update')
+    if (
+      this.state.scrollTop > 355 &&
+      this.state.scrollTop < 2354 &&
+      this.state.projectsButton !== 1
+    ) {
+      this.navUnderline('projects')
+    } else if (
+      this.state.scrollTop > 2354 &&
+      this.state.scrollTop < 2819 &&
+      this.state.skillsButton !== 1
+    ) {
+      this.navUnderline('skills')
+    } else if (this.state.scrollTop > 2820 && this.state.contactButton !== 1) {
+      this.navUnderline('contact')
+    } else if (this.state.scrollTop < 355 && this.state.noButton !== 1) {
+      this.navUnderline('')
+    }
+  }
+
+  componentWillUnmount() {
+    this.state &&
+      window.removeEventListener('scroll', this.onScroll.bind(this), false)
+  }
+
+  onScroll(event) {
+    this.setState({
+      scrollTop: window.scrollY
+    })
+    console.log(window.scrollY)
+  }
+
+  navUnderline(section) {
+    if (section === 'projects') {
+      this.setState({
+        noButton: 0,
+        projectsButton: 1,
+        skillsButton: 0,
+        contactButton: 0,
+        aboutButton: 0
+      })
+    } else if (section === 'skills') {
+      this.setState({
+        noButton: 0,
+        projectsButton: 0,
+        skillsButton: 1,
+        contactButton: 0,
+        aboutButton: 0
+      })
+    } else if (section === 'contact') {
+      this.setState({
+        noButton: 0,
+        projectsButton: 0,
+        skillsButton: 0,
+        contactButton: 1,
+        aboutButton: 0
+      })
+    } else if (section === 'about') {
+      this.setState({
+        noButton: 0,
+        projectsButton: 0,
+        skillsButton: 0,
+        contactButton: 0,
+        aboutButton: 1
+      })
+    } else {
+      this.setState({
+        noButton: 1,
+        projectsButton: 0,
+        skillsButton: 0,
+        contactButton: 0,
+        aboutButton: 0
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div ref="listview" id="nav-container">
+        <a id="nav-name" href="#intro-container">
+          LOREN MARIQUIT
+        </a>
+        <a
+          className={this.state.projectsButton && 'nav-selected'}
+          href="#proj-container"
+          onClick={() => this.navUnderline('projects')}
+        >
+          PROJECTS
+        </a>
+        <a
+          className={this.state.skillsButton && 'nav-selected'}
+          href="#skills-container"
+          onClick={() => this.navUnderline('skills')}
+        >
+          SKILLS
+        </a>
+        <a
+          className={this.state.contactButton && 'nav-selected'}
+          href="#contact-container"
+          onClick={() => this.navUnderline('contact')}
+        >
+          CONTACT
+        </a>
+        <div>|</div>
+        <div
+          className={this.state.aboutButton && 'nav-selected'}
+          onClick={() => this.navUnderline('about')}
+        >
+          ABOUT
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 function clicked() {
