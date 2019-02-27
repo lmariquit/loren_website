c87da96bdf68e036b889e6b6512d6def8aec0de1
Loren Mariquit <l.mariquit@gmail.com>
2019-02-27 15:35:16 -0500

    testing to see if webpack dev server works. I removed the express server and left the start scsript as node server and put the port back in

--- server/index.js
+++ server/index.js
@@ -1,27 +1,27 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+// const path = require('path')
+// const express = require('express')
+// const app = express()
+// const morgan = require('morgan')
+// const bodyParser = require('body-parser')
 
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(morgan('dev'))
+// app.use(express.static(path.join(__dirname, '..', 'public')))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
-})
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+// })
 
-app.use(function(err, req, res, next) {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+// app.use(function(err, req, res, next) {
+//   console.error(err)
+//   console.error(err.stack)
+//   res.status(err.status || 500).send(err.message || 'Internal server error.')
+// })
 
-const port = process.env.PORT || 3000
-app.listen(port, function() {
-  console.log('Knock, knock')
-  console.log("Who's there?")
-  console.log(`Your server, listening on port ${port}`)
-})
+// const port = process.env.PORT || 3000
+// app.listen(port, function() {
+//   console.log('Knock, knock')
+//   console.log("Who's there?")
+//   console.log(`Your server, listening on port ${port}`)
+// })
